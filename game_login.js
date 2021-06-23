c6ee0af938fcdbc22f2530cd8beae9c2ef634140
DanialShirzad2009 <76796719+DanialShirzad2009@users.noreply.github.com>
2021-06-23 10:41:37 -0700

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,7 @@
+function login(){
+    var name1=document.getElementById("player1_name").value;
+    var name2=document.getElementById("player2_name").value;
+    localStorage.setItem("player1_name", name1);
+    localStorage.setItem("player2_name", name2);
+    window.location.replace("game_page.html");
+    }
